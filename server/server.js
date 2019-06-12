a2a3d34332fc6bc296baebe2347bb0676bea4112
Trevorjoel <trevsstuff@hotmail.com>
2019-06-12 17:01:29 +0700

    Separate into server and modules. Left in debug artifacts.

---
+++ server/server.js
@@ -0,0 +1,15 @@
+
+// Setup node server
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`LISTENING on port ${port}`));
+let temperatureModule = require('./temperatureSim');
+
+
+temperatureModule();
+// So this fires off but returns undefined... another promise maybe?
+console.log("FROM THE SERVER FILE: " + temperatureModule.reading);
+let readingPromise = new Promise(function (resolve, reject) {
+  // if (reading)
+});
---
+++ server/temperatureSim.js
@@ -0,0 +1,116 @@
+const moment = require('moment');
+
+
+function temperatureModule()  {
+    let recordId = 0; // Set the unique id
+    let timesToExecute = 5; // Set the number of times the function should execute
+    const frequency = 700; // Set how frequently it should run in milliseconds
+    const max = 26; //  max value for temperature
+    let min = 0; // min value for temperature
+    let reading =[];
+    
+// Main temperature function
+    function temperatureSim() {
+        console.log('This RUNS Temp sim Func');
+        console.log('This RUNS times to execute : ' + timesToExecute);
+    if (timesToExecute !== 0) {
+        console.log('This RUNS Temp sim Func if statement');
+        let temp = randomNumber(max, min);
+        let setStatus = statusSetter(temp);
+        let timeStamp = moment().format('MMMM Do YYYY, h:mm:ss a'); // Month 10th 2019, 5:12:19 pm
+        
+        let temperatureParams = {
+            id: recordId,
+            time: timeStamp,
+            temperature: temp,
+            status: setStatus
+        };
+        reading.push(temperatureParams);
+        temperaturePromise
+            .then(() => { // Probably add a method here that allows interaction of various sensors data
+                console.log('This RUNS on fulfilled promise statement');
+                console.log('Reading(in promise)1 : ' + reading);
+                console.log('Reading(in promise)2 : ' + reading[0].time);
+                let jsonString = JSON.stringify(reading);
+                console.log(jsonString);
+                module.exports.reading = reading;
+            
+            })
+        
+            .catch(function () {
+                console.log('This RUNS catch');
+            });
+        
+        timesToExecute--;
+        recordId++;
+        
+        return reading;
+    } else {
+    
+    
+        
+        clearInterval(tempTimer);
+        console.log('Data finished.');
+    }
+    
+    return reading;
+}
+// Generate random number
+let randomNumber = (max, min) => {
+    return Math.floor(Math.random() * max) + min;
+};
+
+// Set status alert
+function statusSetter(temp) {
+    let st;
+    switch (true) {
+        case temp <= 3 :
+            st = "Red Alert Low temperature!";
+            break;
+        case temp > 3 && temp <= 9 :
+            st = "Yellow advice Low temperature";
+            break;
+        case temp >= 10 && temp <= 18 :
+            st = "Green keep your temperature between 9 & 18 degrees celsius";
+            break;
+        case temp >= 19 && temp <= 23 :
+            st = "Yellow advice high temperature";
+            break;
+        case temp >= 24 :
+            st = "Red Alert: high temperature";
+            break;
+        default:
+            st = 'Cannot read data';
+        
+    }
+    
+    return st;
+}
+
+let temperaturePromise = new Promise(function (resolve, reject) {
+    
+    if (timesToExecute !== 0) {
+        
+        resolve('Promise resolved');
+        console.log('Resolved');
+    } else {
+        reject();
+    }
+});
+    const tempTimer = setInterval(temperatureSim, frequency);
+    
+}
+
+ /*   *TO DO*
+ // After tempTimer ^ runs I would like the returned values of reading Probably as both Json and an object available to be passed into another function.
+ // I want the values of other probe readings to interact.
+ // In practicality, high temperatures also makes ammonia more toxic to fish at lower levels..
+ // This may/may not need to be built into the system but for my own clarity in interested in how to pass
+    the 'reading' object array into other places
+ 
+ */
+
+
+module.exports = temperatureModule;
+
+
